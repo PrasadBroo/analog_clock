3a1c1f3fffa59d12ae4635dc173645fd837f1eae
PrasadBroo <PrasadBroo@users.noreply.github.com>
2020-05-01 18:57:21 +0530

    chnaged

--- main.js
+++ main.js
@@ -4,28 +4,31 @@ const second = document.querySelector('.sec');
 
 
 
-var hourAgle = 30;
-var minAngle = 125;
-var secAngle = 0;
+var hourAgle,minAngle ;
+var secAngle;
 var hmm = setInterval(changeTime,1000);
 
 
-minute.style.transform=`rotate(${minAngle}deg)`;
-hour.style.transform=`rotate(${hourAgle}deg)`;
-second.style.transform=`rotate(${secAngle}deg)`;
+
 
 function changeTime(){
     secAngle+=6;
     second.style.transform=`rotate(${secAngle}deg)`;
 
-   
-    if(secAngle==360){
-        secAngle=0;
-        minAngle+=6;
-        hourAgle+=0.5;
-        minute.style.transform=`rotate(${minAngle}deg)`;
-        hour.style.transform=`rotate(${hourAgle}deg)`;
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
 
-    }
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    hourAgle=hours*30;
+    minAngle=minutes*6;
+    secAngle=date.getSeconds()*6;
+
     
+    minute.style.transform=`rotate(${minAngle}deg)`;
+    hour.style.transform=`rotate(${hourAgle}deg)`;
 }
+
